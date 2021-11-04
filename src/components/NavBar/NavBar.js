5042bd1ac566032fc3b19b2ee5a70add23285c1f
n_tananh <tananh691@gmail.com>
2021-11-05 00:07:25 +0700

    refactor: nav bar, remove unnecessary import

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './NavBar.css'
-import {NavLink} from "react-router-dom";
+import {Link, NavLink} from "react-router-dom";
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
@@ -9,61 +9,61 @@ import {alpha, styled} from '@mui/material/styles';
 import InputBase from '@mui/material/InputBase';
 import logo from '../../assets/images/post_logo.png';
 
-const NavBar = () => {
+const Search = styled('div')(({theme}) => ({
+	position: 'relative',
+	borderRadius: theme.shape.borderRadius,
+	backgroundColor: alpha(theme.palette.common.black, 0.1),
+	'&:hover': {
+		backgroundColor: alpha(theme.palette.common.black, 0.2),
+	},
+	marginLeft: 0,
+	width: '100%',
+	[theme.breakpoints.up('sm')]: {
+		marginLeft: theme.spacing(1),
+		width: 'auto',
+	},
+}));
 
-	const Search = styled('div')(({theme}) => ({
-		position: 'relative',
-		borderRadius: theme.shape.borderRadius,
-		backgroundColor: alpha(theme.palette.common.black, 0.1),
-		'&:hover': {
-			backgroundColor: alpha(theme.palette.common.black, 0.2),
-		},
-		marginLeft: 0,
+const SearchIconWrapper = styled('div')(({theme}) => ({
+	padding: theme.spacing(0, 2),
+	height: '100%',
+	position: 'absolute',
+	pointerEvents: 'none',
+	display: 'flex',
+	alignItems: 'center',
+	justifyContent: 'center',
+}));
+
+// Style for input field
+const StyledInputBase = styled(InputBase)(({theme}) => ({
+	color: '#fff',
+	'& .MuiInputBase-input': {
+		padding: theme.spacing(1, 1, 1, 0),
+		// vertical padding + font size from searchIcon
+		paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+		transition: theme.transitions.create('width'),
 		width: '100%',
 		[theme.breakpoints.up('sm')]: {
-			marginLeft: theme.spacing(1),
-			width: 'auto',
-		},
-	}));
-
-	const SearchIconWrapper = styled('div')(({theme}) => ({
-		padding: theme.spacing(0, 2),
-		height: '100%',
-		position: 'absolute',
-		pointerEvents: 'none',
-		display: 'flex',
-		alignItems: 'center',
-		justifyContent: 'center',
-	}));
-
-	// Style for input field
-	const StyledInputBase = styled(InputBase)(({theme}) => ({
-		color: '#fff',
-		'& .MuiInputBase-input': {
-			padding: theme.spacing(1, 1, 1, 0),
-			// vertical padding + font size from searchIcon
-			paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-			transition: theme.transitions.create('width'),
-			width: '100%',
-			[theme.breakpoints.up('sm')]: {
-				width: '12ch',
-				'&:focus': {
-					width: '20ch',
-				},
+			width: '12ch',
+			'&:focus': {
+				width: '20ch',
 			},
 		},
-	}));
+	},
+}));
 
+const NavBar = () => {
 	return (
 		<div className="nav__wrapper">
 			<Box sx={{flexGrow: 1}}>
 				{/*Start nav bar*/}
 				<AppBar position="static" sx={{background: "transparent", boxShadow: "none"}}>
 					<Toolbar sx={{justifyContent: "space-around"}}>
-
 						{/*Start Logo*/}
 						<div className="nav__logo">
-							<img src={logo} alt="logo"/>
+							<Link to="/">
+								<img src={logo} alt="logo"/>
+							</Link>
 						</div>
 						{/*End Logo*/}
 
@@ -87,7 +87,6 @@ const NavBar = () => {
 							{/*End search field*/}
 						</div>
 						{/*End Link and Search bar*/}
-
 					</Toolbar>
 				</AppBar>
 				{/*End nav bar*/}
