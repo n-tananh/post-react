d1844c92c8b1b1f617e6343f3e83bd5740ef3b4c
n_tananh <tananh691@gmail.com>
2021-11-05 11:33:22 +0700

    style: done style for contact page

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,34 +1,59 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './Contact.css'
 import '../../common/common.css'
 import {useForm} from "react-hook-form";
+import {TextField} from "@mui/material";
 
 // TODO contact form
 const Contact = () => {
-	const { register, handleSubmit, formState: { errors } } = useForm();
-	const onSubmit = data => console.log(data);
+	const {register, handleSubmit, formState: {errors}} = useForm();
 
-	const [message, setMessage] = useState("");
-
-	const handleTyping = (e) => {
-		setMessage(e.target.value)
+	const onSubmit = () => {
+		console.log("submit");
 	}
-
-	console.log(message);
 
 	return (
 		<div className="contact__wrapper">
-			<p>Want to get in touch? Fill out the form below to send me a message and I will get back to you as soon as possible!</p>
-			<div className="contact__form">
-				<form onSubmit={handleSubmit(onSubmit)}>
-					<input defaultValue="test" {...register("example")} />
-					<input {...register("exampleRequired", { required: true })} />
-					{errors.exampleRequired && <span>This field is required</span>}
-					<input defaultValue="test" {...register("example")} />
-					<textarea onChange={ handleTyping } value={message}/>
+			<div className="contact__content">
+				<p>Want to get in touch? Fill out the form below to send me a message and I will get back to you as soon as
+					possible!</p>
+				<div className="contact__form">
+					<form onSubmit={handleSubmit(onSubmit)}>
+						<TextField
+							id="name"
+							label="Name"
+							margin="normal"
+							className="form__input"
+							variant="standard"
+						/>
+						<TextField
+							id="email"
+							label="Email Address"
+							margin="normal"
+							className="form__input"
+							variant="standard"
+						/>
+						<TextField
+							id="phone-num"
+							label="Phone Number"
+							margin="normal"
+							className="form__input"
+							variant="standard"
+						/>
+						<TextField
+							id="message"
+							label="Messages"
+							margin="normal"
+							className="form__input"
+							multiline
+							variant="standard"
+							minRows={8}
+							maxRows={8}
+						/>
 
-					<input type="submit" className="contact btn__link" value="Send"/>
-				</form>
+						<input type="submit" className="contact btn__link" value="Send"/>
+					</form>
+				</div>
 			</div>
 		</div>
 	);
